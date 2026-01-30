b749491992e74dffa7f55606916a78fb94d18db1
Wouter Haringsma <63634970+wouterharingsma@users.noreply.github.com>
2026-01-30 20:31:35 +0100

    Remove leftover files

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Activate Python virtual environment and start the API in the background
-. /home/node/.n8n/python/venv/bin/activate && \
-python3 -m uvicorn src.api_server:app --host 0.0.0.0 --port 8000 &
-
-# Start n8n in the foreground
-n8n start
